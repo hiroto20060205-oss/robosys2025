c8695d91fafbdb846a4c60c8493c4c8cd491ba50
Fujitake Hiroto <hiroto180205@icloud.com>
2025-11-26 09:47:58 +0900

    計算できない値を入力した場合を追加

--- test.bash
+++ test.bash
@@ -13,5 +13,10 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" =1 ]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "${res}" = 0 ] && echo ok
 exit $res
