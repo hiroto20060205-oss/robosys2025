d9f0e5cc730bbc8c155625a50548090fbde5c28a
Fujitake Hiroto <hiroto180205@icloud.com>
2025-11-30 20:21:44 +0900

    課題用に書き換え(ただしエラーあり)

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Fujitake Hiroto
-# SPDX-Licence-Identifier: BSD-3-Clause
+# SPDX-Licence-Identifier: MIT
 
 ng () {
 	echo ${1}行目が違うよ
@@ -10,17 +10,9 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$(seq 6 | ./prime_factorization.py) #素数に分解されるか確認
+[ "${out}" = "[2, 3]" ] || ng "$LINENO"
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok
 exit $res
