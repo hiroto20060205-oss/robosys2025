f298862a20ddbc9aaf215d1c6b5ef912b080af85
Fujitake Hiroto <hiroto180205@icloud.com>
2025-11-30 20:33:22 +0900

    数字以外を入力した場合の対処法を追加

--- test.bash
+++ test.bash
@@ -13,6 +13,10 @@ res=0
 out=$(echo 6 | ./prime_factorization.py) #素数に分解されるか確認
 [ "${out}" = "[2, 3]" ] || ng "$LINENO"
 
+### STRANGE INPUT ###
+out=$(echo あ | ./prime_factorization.py) #数字以外を入力したらエラーを出すか
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok
 exit $res
