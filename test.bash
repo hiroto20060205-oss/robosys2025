84b02a38091fe86b1d8d7dfc152307d56b9cb82a
Fujitake Hiroto <hiroto180205@icloud.com>
2025-12-24 22:18:22 +0900

    負の値を絶対値で返す

--- test.bash
+++ test.bash
@@ -9,19 +9,23 @@ ng () {
 
 res=0
 
-#正常な入力
-
+#周波数を周期に変換
 # 1 / 1000 = 0.001s = 1.0ms
-out=$(echo 1000 | ./convert_frequency_period) #周波数を周期に変換
+out=$(echo 1000 | ./convert_frequency_period) 
 [ "${out}" = "1 ms" ] || ng "$LINENO"
 
+#周期を周波数に変換
 # 1 / 0.001 = = 1000Hz = 1.0kHz
-out=$(echo 0.001 | ./convert_frequency_period) #周期を周波数に変換
+out=$(echo 0.001 | ./convert_frequency_period) 
 [ "${out}" = "1 kHz" ] || ng "$LINENO"
 
 # 無限大の処理
 out=$(echo 0 | ./convert_frequency_period) 
-[ "${out}" = "Infinite" ] || ng "$LINENO"
+[ "${out}" = "Infinity" ] || ng "$LINENO"
+
+#負の値を絶対値で出力
+out=$(echo -1000 | ./convert_frequency_period)
+[ "${out}" = "1 ms" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok
 exit $res
