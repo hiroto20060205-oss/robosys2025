2b2cb9a9b6c84b62f1c417328ec75b61f03b6805
Fujitake Hiroto <hiroto180205@icloud.com>
2025-12-24 20:02:38 +0900

    Recreate test.bash

--- test.bash
+++ test.bash
@@ -9,30 +9,7 @@ ng () {
 
 res=0
 
-### NORMAL INPUT ###
-out=$(echo 6 | ./prime_factorization.py) #素数に分解されるか確認
-[ "${out}" = "[2, 3]" ] || ng "$LINENO"
 
-out=$(echo 7 | ./prime_factorization.py) #単体で素数の数が表示されるか確認
-[ "${out}" = "[7]" ] || ng "$LINENO"
-
-### STRANGE INPUT ###
-out=$(echo あ | ./prime_factorization.py) #数字以外を入力したらエラーを出すか
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./prime_factorization.py) #空文字を入力したらエラーを出すか
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-### It's not an error, but it's not normal ###
-out=$(echo 1 | ./prime_factorization.py) #2で割り切れない数字を入力したら[]を出すか
-[ "$?" = 0 ]        || ng "$LINENO"
-[ "${out}" = "[]" ] || ng "$LINENO"
-
-out=$(echo -6  | ./prime_factorization.py) #負の値を入力したら[]を出すか
-[ "$?" = 0 ]        || ng "$LINENO"
-[ "${out}" = "[]" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok
 exit $res
